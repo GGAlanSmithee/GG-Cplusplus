d0bee5b1bb9e4169fb9888853fcb38476e17508f
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-19 15:00:46 +0000

    Updated comments to include date and added missing comments in graphics.h

--- src/Graphics/Graphics.h
+++ src/Graphics/Graphics.h
@@ -21,49 +21,67 @@ namespace GGGraphics
     extern GLuint VBO;
     extern GLuint IBO;
 
-    /// @author Alan Smithee
     /// Initializes SDL video and OpenGL
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void Initialize();
 
-    /// @author Alan Smithee
     /// Creates a vertexbuffer by generating and binding a buffer with vertex data
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void CreateVertexBuffer();
 
-    /// @author Alan Smithee
     /// Creates an indexbuffer by generating and binding a buffer with index data
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void CreateIndexBuffer();
 
-    /// @author Alan Smithee
     /// Sets the swap interval of the screen
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     /// @param interval the new swap interval
     /// @remarks 1.0 will sync the swap interval with the screens refresh rate
     void SetSwapInterval(const int);
 
-    /// @author Alan Smithee
     /// Sets the screen clear color
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     /// @param r red value
     /// @param g green value
     /// @param b blue value
     /// @param a alpha value
     void SetClearColor(const float, const float, const float, const float);
 
-    /// @author Alan Smithee
     /// Sets the scale of the next object to be processed
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     /// @param scale the new scale
     void SetScale(const float);
 
+    /// @author Alan Smithee
+    /// @date 2014-11-19
+    /// Draws a model to the screen using current context
+    /// @param model the model to draw
     void DrawModel(const glm::mat4&);
 
+    /// Clears the screen
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void ClearScreen();
 
+    /// Updates the screen by swapping it according to the set swap interval
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void UpdateScreen();
 
-    /// @author Alan Smithee
     /// Cleans up used resources by destroying the glContext and deleting the SDL_Window* used
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     void CleanUp();
 
-    /// @author Alan Smithee
     /// Returns a boolean value indicating if SDL video and OpenGL was initialized
+    /// @author Alan Smithee
+    /// @date 2014-11-19
     /// @return true if initialized succesfully, false if not
     const bool WasInitialized();
 }
