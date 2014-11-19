01702c0230c4f3a4cb7310b86f5246470904b67c
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-19 15:05:15 +0000

    Added comments to components

--- src/Component/Component.h
+++ src/Component/Component.h
@@ -6,6 +6,9 @@
 
 namespace GGComponent
 {
+    /// @author Alan Smithee
+    /// @date 2014-11-19
+    /// Enum representing the different component types
     enum class Type : int
     {
         None       = 0,
@@ -14,12 +17,18 @@ namespace GGComponent
         Appearance = 1 << 2
     };
 
+    /// @author Alan Smithee
+    /// @date 2014-11-19
+    /// Contains information of the appearance of any entity using this component
     typedef struct Appearance
     {
         std::string TextureId    = "";
     }
     Appearance;
 
+    /// @author Alan Smithee
+    /// @date 2014-11-19
+    /// Contains information of the model information of any entity using this component
     typedef struct Model
     {
         glm::mat4 Scale       = glm::mat4(1.0);
@@ -32,7 +41,10 @@ namespace GGComponent
         }
     }
     Model;
-
+    
+    /// @author Alan Smithee
+    /// @date 2014-11-19
+    /// Contains information of the physical attributes of any entity using this component
     typedef struct Physics
     {
         glm::vec2 Velocity = glm::vec2(1.0f);
