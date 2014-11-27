12476e3449a8a186a53ae2d051d782da3b7b4512
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-27 10:13:51 +0000

    adds comments to enum.h

--- src/Enum/Enum.h
+++ src/Enum/Enum.h
@@ -3,12 +3,19 @@
 
 namespace GGEnum
 {
+    /// Enumeration of different shader uniforms
+    /// @author Alan Smithee
+    /// @date created 2014-11-27
     enum class Uniform : int
     {
         MVP,
         TextureSampler
     };
 
+    /// Enumeration of engine defined textures
+    /// @author Alan Smithee
+    /// @date created 2014-11-27
+    /// @todo when implementing texture batching, this might be removed in favor of a map of texture (texture name / texture)
     enum class Texture : int
     {
         Crate
