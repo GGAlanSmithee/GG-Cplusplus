e720405d7ee395d76fbb4f44439c853fd4733e00
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-28 10:02:22 +0000

    adds comments to logging.h

--- src/Engine/Renderer/Logging.h
+++ src/Engine/Renderer/Logging.h
@@ -7,19 +7,54 @@
 
 namespace GGRendererEngine
 {
+    /// Holds the last encountered error
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
+    /// @todo this needs to be refactored to be encapsulated
     extern std::string error;
 
+    /// Sets an error text
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     void SetError(const char*, const char* = "");
+    
+    /// Sets an error text
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     void SetError(const char*, const unsigned char*);
+    
+    /// Sets an error text
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     void SetError(const char*, const std::string&);
+    
+    /// Sets an error text
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     void SetError(const char*, const std::vector<char>&);
+    
+    /// Sets an error text
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     void SetError(const char*, const int);
 
+    /// Retrieves the info log from a shader
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
+    /// @param shader the id of the sahder to get the log for
+    /// @return an errormessage as a char vector
     const std::vector<GLchar> GetShaderInfoLog(const GLuint);
+    
+    /// Retrieves the info log from a shader program
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
+    /// @param program the id of the program to get the log for
+    /// @return an errormessage as a char vector
     const std::vector<GLchar> GetProgramInfoLog(const GLuint);
 
-    /// @author Alan Smithee
     /// Gets the latest recorded error from within the GGGraphics namespace
+    /// @author Alan Smithee
+    /// @date created 2014-11-28
     /// @return an errormessage as a std::string
     const std::string GetError();
 }
