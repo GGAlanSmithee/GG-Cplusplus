65386db683d2fa132a133965bca5225969d83092
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-28 18:31:17 +0100

    adds functionality for saving a basic gg model file

--- ColladaModelLoading/Main.cpp
+++ ColladaModelLoading/Main.cpp
@@ -3,22 +3,71 @@
 
 int main()
 {
-    tinyxml2::XMLDocument doc;
-    doc.LoadFile("crate.dae");
+    tinyxml2::XMLDocument colladaDoc;
+    colladaDoc.LoadFile("crate.dae");
 
-    auto root = doc.FirstChildElement("COLLADA");
+    auto colladaRoot = colladaDoc.FirstChildElement("COLLADA");
 
-    if (root == nullptr)
+    if (colladaRoot == nullptr)
     {
-        std::cout << "is null" << std::endl;
+        std::cerr << "Could not load model" << std::endl;
+
+        return -1;
     }
 
-    auto e = root->FirstChildElement("library_geometries");
+    tinyxml2::XMLDocument ggDoc;
 
-    while (e != nullptr)
+	auto declaration = ggDoc.NewDeclaration("xml version=\"1.0\"");
+	ggDoc.InsertFirstChild(declaration);
+
+    auto result = ggDoc.SaveFile("model.gg");
+
+    if (result != tinyxml2::XML_SUCCESS)
     {
-        std::cout << "library geometries" << std::endl;
-        e = e->NextSiblingElement("library_geometries");
+        std::cerr << "Could not save model XML data: " << result << std::endl;
+    }
+
+    auto libraryGeometries = colladaRoot->FirstChildElement("library_geometries");
+
+    while (libraryGeometries != nullptr)
+    {
+//        tinyxml2::XMLElement* geometriesElement = new tinyxml2::XMLElement("geometries");
+//        ggDoc.InsertFirstChild(geometriesElement);
+
+        auto geometry = libraryGeometries->FirstChildElement("geometry");
+
+        while (geometry != nullptr)
+        {
+//            tinyxml2::XMLElement* geometryElement = new tinyxml2::XMLElement("geometries");
+//            geometriesElement->InsertFirstChild(geometryElement);
+
+            auto mesh = geometry->FirstChildElement("mesh");
+
+            while (mesh != nullptr)
+            {
+                auto source = mesh->FirstChildElement("source");
+
+                while (source != nullptr)
+                {
+                    std::cout << "Source id: " << source->Attribute("id") << std::endl;
+
+                    source = source->NextSiblingElement("source");
+                }
+
+                auto polyList = mesh->FirstChildElement("polylist");
+
+                if (polyList != nullptr)
+                {
+                    std::cout << "Number of indicies: " << polyList->Attribute("count") << std::endl;
+                }
+
+                mesh = mesh->NextSiblingElement("mesh");
+            }
+
+            geometry = geometry->NextSiblingElement("geometry");
+        }
+
+        libraryGeometries = libraryGeometries->NextSiblingElement("library_geometries");
     }
 
     return 0;
