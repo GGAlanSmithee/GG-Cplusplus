dd804e1b1d6813ffbcdd8ca1e880ffbba06c6a56
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-11-28 19:32:53 +0100

    changes the collada to gg model converter to include all basic information needed to render the model

--- ColladaModelLoading/Main.cpp
+++ ColladaModelLoading/Main.cpp
@@ -3,15 +3,25 @@
 
 int main()
 {
+    std::string colladaFileName;
+
+    std::cout << "Enter the name of the collada file to load (without .dae): ";
+    std::cin >> colladaFileName;
+
     tinyxml2::XMLDocument colladaDoc;
-    colladaDoc.LoadFile("crate.dae");
+    auto loadingResult = colladaDoc.LoadFile((colladaFileName + ".dae").c_str());
+
+    if (loadingResult != tinyxml2::XML_SUCCESS)
+    {
+        std::cerr << "Could not load model" << std::endl;
+        return -1;
+    }
 
     auto colladaRoot = colladaDoc.FirstChildElement("COLLADA");
 
-    if (colladaRoot == nullptr)
+	if (colladaRoot == nullptr)
     {
-        std::cerr << "Could not load model" << std::endl;
-
+        std::cerr << "The loaded model is not a valid collada model" << std::endl;
         return -1;
     }
 
@@ -20,54 +30,99 @@ int main()
 	auto declaration = ggDoc.NewDeclaration("xml version=\"1.0\"");
 	ggDoc.InsertFirstChild(declaration);
 
-    auto result = ggDoc.SaveFile("model.gg");
+    auto colladaGeometries = colladaRoot->FirstChildElement("library_geometries");
+
+    while (colladaGeometries != nullptr)
+    {
+        auto ggGeometries = ggDoc.NewElement("geometries");
+        ggDoc.InsertEndChild(ggGeometries);
+
+        auto colladaGeometry = colladaGeometries->FirstChildElement("geometry");
+
+        while (colladaGeometry != nullptr)
+        {
+            auto geometryName = std::string(colladaGeometry->Attribute("id"));
+
+            auto ggGeometry = ggDoc.NewElement("geometry");
+            ggGeometries->InsertEndChild(ggGeometry);
+
+            auto colladaMesh = colladaGeometry->FirstChildElement("mesh");
+
+            while (colladaMesh != nullptr)
+            {
+                auto ggMesh = ggDoc.NewElement("mesh");
+                ggGeometry->InsertEndChild(ggMesh);
+
+                auto colladaSource = colladaMesh->FirstChildElement("source");
+
+                while (colladaSource != nullptr)
+                {
+                    if (std::string(colladaSource->Attribute("id")).compare(geometryName + "-positions") == 0)
+                    {
+                        auto floatArray = colladaSource->FirstChildElement("float_array");
+                        auto accessor = colladaSource->FirstChildElement("technique_common")
+                                                   ->FirstChildElement("accessor");
+
+                        auto count = floatArray->Attribute("count");
+                        auto stride = accessor->Attribute("stride");
+                        auto strideCount = accessor->Attribute("count");
+
+                        auto ggPositions = ggDoc.NewElement("positions");
+                        ggPositions->SetText(floatArray->GetText());
+                        ggPositions->SetAttribute("count", count);
+                        ggPositions->SetAttribute("stride", stride);
+                        ggPositions->SetAttribute("stride-count", strideCount);
+                        ggMesh->InsertEndChild(ggPositions);
+                    }
+                    else if (std::string(colladaSource->Attribute("id")).compare(geometryName + "-normals") == 0)
+                    {
+                        auto floatArray = colladaSource->FirstChildElement("float_array");
+                        auto accessor = colladaSource->FirstChildElement("technique_common")
+                                                   ->FirstChildElement("accessor");
+
+                        auto count = floatArray->Attribute("count");
+                        auto stride = accessor->Attribute("stride");
+                        auto strideCount = accessor->Attribute("count");
+
+                        auto ggNormals = ggDoc.NewElement("normals");
+                        ggNormals->SetText(floatArray->GetText());
+                        ggNormals->SetAttribute("count", count);
+                        ggNormals->SetAttribute("stride", stride);
+                        ggNormals->SetAttribute("stride-count", strideCount);
+                        ggMesh->InsertEndChild(ggNormals);
+                    }
+
+                    colladaSource = colladaSource->NextSiblingElement("source");
+                }
+
+                auto polyList = colladaMesh->FirstChildElement("polylist");
+
+                if (polyList != nullptr)
+                {
+                    auto ggIndices = ggDoc.NewElement("inidces");
+                    ggIndices->SetAttribute("count", polyList->Attribute("count"));
+                    ggIndices->SetText(polyList->FirstChildElement("p")->GetText());
+                    ggMesh->InsertEndChild(ggIndices);
+                }
+
+                colladaMesh = colladaMesh->NextSiblingElement("mesh");
+            }
+
+            colladaGeometry = colladaGeometry->NextSiblingElement("geometry");
+        }
+
+        colladaGeometries = colladaGeometries->NextSiblingElement("library_geometries");
+    }
+
+    std::string ggModelName;
+    std::cout << "Enter the name of the file to save: ";
+    std::cin >> ggModelName;
+
+    auto result = ggDoc.SaveFile((ggModelName + ".gg").c_str());
 
     if (result != tinyxml2::XML_SUCCESS)
     {
         std::cerr << "Could not save model XML data: " << result << std::endl;
-    }
-
-    auto libraryGeometries = colladaRoot->FirstChildElement("library_geometries");
-
-    while (libraryGeometries != nullptr)
-    {
-//        tinyxml2::XMLElement* geometriesElement = new tinyxml2::XMLElement("geometries");
-//        ggDoc.InsertFirstChild(geometriesElement);
-
-        auto geometry = libraryGeometries->FirstChildElement("geometry");
-
-        while (geometry != nullptr)
-        {
-//            tinyxml2::XMLElement* geometryElement = new tinyxml2::XMLElement("geometries");
-//            geometriesElement->InsertFirstChild(geometryElement);
-
-            auto mesh = geometry->FirstChildElement("mesh");
-
-            while (mesh != nullptr)
-            {
-                auto source = mesh->FirstChildElement("source");
-
-                while (source != nullptr)
-                {
-                    std::cout << "Source id: " << source->Attribute("id") << std::endl;
-
-                    source = source->NextSiblingElement("source");
-                }
-
-                auto polyList = mesh->FirstChildElement("polylist");
-
-                if (polyList != nullptr)
-                {
-                    std::cout << "Number of indicies: " << polyList->Attribute("count") << std::endl;
-                }
-
-                mesh = mesh->NextSiblingElement("mesh");
-            }
-
-            geometry = geometry->NextSiblingElement("geometry");
-        }
-
-        libraryGeometries = libraryGeometries->NextSiblingElement("library_geometries");
     }
 
     return 0;
