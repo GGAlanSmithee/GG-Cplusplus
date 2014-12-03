4c46330442eee5af89766e65d88e19a4263e828f
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-12-03 09:53:08 +0000

    adds manager.h file and introduces the concept of managers to the engine

---
+++ src/Engine/Core/Manager.h
@@ -0,0 +1,29 @@
+namespace GGCore
+{
+    /// Abstract baseclass for managers. A manager is a class that provides logic which affects some part of the game that isn't component specific
+    /// @author Alan Smithee
+    /// @date created 2014-12-03
+    class Manager
+    {
+        public:
+            /// Initializes the manager
+            /// @author Alan Smithee
+            /// @date created 2014-12-03
+            Manager();
+            
+            /// Virtual destructor for inheritance purposes
+            /// @author Alan Smithee
+            /// @date created 2014-12-03
+            virtual ~Manager() = 0;
+            
+            /// Is called every frame  from game loop on update
+            /// @author Alan Smithee
+            /// @date created 2014-12-03
+            virtual void OnUpdate();
+            
+            /// Is called at a fixed update rate from game loop on update
+            /// @author Alan Smithee
+            /// @date created 2014-12-03
+            virtual void OnFixedUpdate();
+    }
+}
