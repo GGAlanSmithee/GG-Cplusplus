1d61369c37e57f37879415317ebe04afcdb262b1
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-12-03 13:16:08 +0000

    changes comment on game object for clearer documentation

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -3,11 +3,10 @@
 
 namespace GGGame
 {
-    /// Represents a game instance and serves as the main starting point of any game
-    /// using the engine
+    /// Represents a game instance and serves as the main starting point of any game using the GGEngine
     /// @author Alan Smithee
     /// @date created 2014-11-25
-    /// @date changed 2014-11-27
+    /// @date changed 2014-12-03
     class Game
     {
         public:
