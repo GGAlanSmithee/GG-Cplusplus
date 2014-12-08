19637f560e40a0f9f3593837ae376eb3951b9bba
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-12-08 09:38:31 +0000

    adds event variable and key up / key down functions

--- src/Engine/Input/Input.h
+++ src/Engine/Input/Input.h
@@ -1,15 +1,33 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 
+#include <SDL.h>
 #include <memory>
 
 namespace GGInputEngine
 {
+	/// Listens to and polls events
+	/// @author Alan Smithee
+	/// @date created 2014-12-08
+	extern SDL_Event* event;
+	
 	/// Polls for events and dispatches them to an event manager
 	/// @author Alan Smithee
 	/// @date created 2014-12-08
 	/// @param eventMananger user defined event manager to be invoked on each event
-	void HandleEvents(const std::unique_ptr<GGCoreEngine::EventManager>);
+	void HandleEvents(const std::unique_ptr<GGCoreEngine::EventManager>&);
+	
+	/// Catches and dispatches all key down events
+	/// @author Alan Smithee
+	/// @date created 2014-12-08
+	/// @param eventMananger user defined event manager to be invoked on key down events
+	void OnKeyDown(const std::unique_ptr<GGCoreEngine::EventManager>&);
+	
+	/// Catches and dispatches all key down events
+	/// @author Alan Smithee
+	/// @date created 2014-12-08
+	/// @param eventMananger user defined event manager to be invoked on key down events
+	void OnKeyUp(const std::unique_ptr<GGCoreEngine::EventManager>&);
 }
 
 #endif // INPUT_H_INCLUDED
