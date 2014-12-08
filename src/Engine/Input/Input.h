9c55d13e2f393d5de72f861dc110809e17ac1026
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-12-08 07:54:06 +0000

    adds declaration of HandleEvents function to the InputEngine namespace

--- src/Engine/Input/Input.h
+++ src/Engine/Input/Input.h
@@ -1,9 +1,15 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 
+#include <memory>
+
 namespace GGInputEngine
 {
-
+	/// Polls for events and dispatches them to an event manager
+	/// @author Alan Smithee
+	/// @date created 2014-12-08
+	/// @param eventMananger user defined event manager to be invoked on each event
+	void HandleEvents(const std::unique_ptr<GGCoreEngine::EventManager>);
 }
 
 #endif // INPUT_H_INCLUDED
