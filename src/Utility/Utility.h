175abc07fcb837d8216869fe066c4af7621e564c
Alan Smithee <ggnore.alan.smithee@gmail.com>
2014-12-15 12:40:51 +0000

    moves split and toints / tofloats functions to the utility namespace

--- src/Utility/Utility.h
+++ src/Utility/Utility.h
@@ -2,6 +2,9 @@
 #define UTILITY_H_INCLUDED
 
 #include <string>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace GGUtility
 {
@@ -25,6 +28,67 @@ namespace GGUtility
     const int ToIntegral(E e)
     {
         return ToUnderlying(e);
+    }
+
+    /// Splits a string by a character into a vector of substrings 
+    /// @author Alan Smithee
+    /// @date created 2014-12-15
+    /// @param s the string to split
+    /// @param delim the delimeter to split the string at
+    /// @return a vector of substrings
+    std::vector<std::string> Split(const std::string& s, char delim)
+    {
+        std::vector<std::string> elems;
+
+        std::stringstream ss(s);
+        std::string item;
+
+        while (std::getline(ss, item, delim))
+        {
+            elems.push_back(item);
+        }
+
+        return elems;
+    }
+
+    /// Converts a space separated string of int values to an array of ints
+    /// @author Alan Smithee
+    /// @date created 2014-12-15
+    /// @param s the string to split
+    /// @return a vector of ints
+    /// @remark the string is split using the Split function, and then converted to integers
+    std::vector<int> ToInts(const std::string& s)
+    {
+        auto partStrings = Split(s, ' ');
+    
+        std::vector<int> ints;
+    
+        for (auto ps : partStrings)
+        {
+            ints.push_back(std::stoi(ps));
+        }
+    
+        return ints;
+    }
+
+    /// Converts a space separated string of float values to an array of floats
+    /// @author Alan Smithee
+    /// @date created 2014-12-15
+    /// @param s the string to split
+    /// @return a vector of floats
+    /// @remark the string is split using the Split function, and then converted to floats
+    std::vector<float> ToFloats(const std::string& s)
+    {
+        auto partStrings = Split(s, ' ');
+    
+        std::vector<float> floats;
+    
+        for (auto ps : partStrings)
+        {
+            floats.push_back(std::stof(ps));
+        }
+    
+        return floats;
     }
 }
 
