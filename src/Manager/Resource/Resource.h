17df20f0bb8bcfe1c9cc4382f47fb969c883dcd1
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-01-08 11:48:30 +0000

    adds comments to resourcemanager.h

--- src/Manager/Resource/Resource.h
+++ src/Manager/Resource/Resource.h
@@ -14,11 +14,37 @@
 
 namespace GGResourceManager
 {
+    /// Indicates if a collada model was loaded
+    /// @author Alan Smithee
+    /// @date created 20154-01-08
+    /// @return true if the model was loaded, false if not
     const bool ColladaModelWasImported();
+    
+    /// Indicates if a gg model was loaded
+    /// @author Alan Smithee
+    /// @date created 20154-01-08
+    /// @return true if the model was loaded, false if not
     const bool GGModelWasImported();
 
+    /// Imports a collada model
+    /// @author Alan Smithee
+    /// @date created 20154-01-08
+    /// @param modelName the name of the model to import
+    /// @return a vector of geometries
     const std::vector<GGGraphics::Geometry> ImportColladaModel(const std::string&);
+    
+    /// Imports a gg model
+    /// @author Alan Smithee
+    /// @date created 20154-01-08
+    /// @param modelName the name of the model to import
+    /// @return a vector of geometries
     const std::vector<GGGraphics::Geometry> ImportGGModel(const std::string&);
+    
+    /// Exports a gg model
+    /// @author Alan Smithee
+    /// @date created 20154-01-08
+    /// @param geometries vector of geometries to export
+    /// @param modelName the name to save the exported model as
     void ExportGGModel(const std::vector<GGGraphics::Geometry>&, const std::string&);
 }
 
