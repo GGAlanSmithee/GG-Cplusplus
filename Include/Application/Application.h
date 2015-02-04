006d25415935f2803fa1d9a19bcbcab92d4ab9cc
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-02-04 09:16:51 +0000

    updates Application.h to use new naming conventions

--- Include/Application/Application.h
+++ Include/Application/Application.h
@@ -1,33 +1,29 @@
 #ifndef APPLICATION_APPLICATION_H_INCLUDED
 #define APPLICATION_APPLICATION_H_INCLUDED
 
-#include <memory>
 #include "Engine/Core/World.h"
 
-namespace GGApplication
+/// Represents an application instance and serves as the main starting point of any application using the GGEngine
+/// @author Alan Smithee
+/// @date created 2014-11-25
+/// @date changed 2015-02-04
+class GG_Application
 {
-     /// Represents an application instance and serves as the main starting point of any application using the GGEngine
-    /// @author Alan Smithee
-    /// @date created 2014-11-25
-    /// @date changed 2014-12-03
-    class Application
-    {
-        public:
-            /// Default constructor for initializing a Application instance
-            /// @author Alan Smithee
-            /// @date created 2014-11-25
-            /// @param world the world holding enteties that live withing the scope of the application
-            Application(const GGCoreEngine::World&);
+    public:
+        /// Default constructor for initializing a Application instance
+        /// @author Alan Smithee
+        /// @date created 2014-11-25
+        /// @param world the world holding enteties that live withing the scope of the application
+        GG_Application(const GG_World&);
 
-            /// Gets the applications world
-            /// @author Alan Smithee
-            /// @date created 2014-11-25
-            /// @return the applications world
-            const GGCoreEngine::World GetWorld() const;
+        /// Gets the applications world
+        /// @author Alan Smithee
+        /// @date created 2014-11-25
+        /// @return the applications world
+        const GG_World GetWorld() const;
 
-        private:
-            GGCoreEngine::World _world;
-    };
-}
+    private:
+        GG_World _world;
+};
 
 #endif // APPLICATION_APPLICATION_H_INCLUDED
