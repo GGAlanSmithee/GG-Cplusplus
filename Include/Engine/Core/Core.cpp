ed43c96a396dfe564a8e9ec4663cf3d17d0612d3
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-02-04 12:13:30 +0000

    updates Core.cpp to use new naming conventions as well as usages of the GG_Event class in the core engine

--- Include/Engine/Core/Core.cpp
+++ Include/Engine/Core/Core.cpp
@@ -6,91 +6,82 @@
 #include "Engine/Event/Event.h"
 #include "Manager/Texture/Texture.h"
 #include "Loader/Loader.h"
+#include "Utility/Exception.h"
 
-namespace GGCoreEngine
+void GG_InitializeSDL(const int flags)
 {
-    const bool InitializeSDL(const int flags)
+    auto result = SDL_Init(flags);
+    
+    if (result != 0)
     {
-        if (SDL_Init(flags) != 0)
-        {
-            std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
-            return false;
-        }
-
-        return true;
+        throw init_error(std::string("Failed to initialize SDL: ") + SDL_GetError());
     }
+}
 
-    const bool InitializeSDLImage(const int flags)
+void GG_InitializeSDLImage(const int flags)
+{
+    auto result = IMG_Init(flags);
+    
+    if (result & flags != flags)
     {
-        if (!(IMG_Init(flags) & flags))
-        {
-            std::cerr << "Failed to initialize SDL_Image: " << IMG_GetError() << std::endl;
-            return false;
-        }
-
-        return true;
+        throw init_error(std::string("Failed to initialize SDL: ") + IMG_GetError());
     }
+}
 
-    const int Execute(GGApplication::Application& application)
-    {
-        auto window = SDL_CreateWindow("Hello, World!",
+const int GG_Execute(GG_Application& application)
+{
+    auto window = SDL_CreateWindow("Hello, World!",
                                    SDL_WINDOWPOS_UNDEFINED,
                                    SDL_WINDOWPOS_UNDEFINED,
                                    640,
                                    480,
                                    SDL_WINDOW_ALLOW_HIGHDPI);
 
-        if (window == nullptr)
-        {
-            std::cerr << "Failed to create window" << std::endl;
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create window" << std::endl;
+        return -1;
+    }
 
-            return -1;
-        }
+    auto renderer = GGRendererEngine::Create(window);
 
-        auto renderer = GGRendererEngine::Create(window);
-
-        if (renderer == nullptr)
-        {
-            std::cerr << "Failed to create renderer" << std::endl;
-
-            SDL_DestroyWindow(window);
-            window = nullptr;
-
-            return -1;
-        }
-
-        auto textureManager = GGTextureManager::Create();
-
-        auto handle = GGTextureManager::AddTexture(textureManager,
-                                                   GGLoader::LoadTexture(GGRendererEngine::GetRenderer(renderer),
-                                                                         "test.png"));
-
-        auto event = GGEventEngine::Create();
-
-        auto running = true;
-
-        GGEventEngine::RegisterCallbackToKeyboardEvent(event, SDLK_ESCAPE, [&]() { running = false; });
-
-        while (running)
-        {
-            GGEventEngine::HandleEvents(event);
-
-            auto texture = GGTextureManager::GetTexture(textureManager, handle);
-            GGRendererEngine::Render(renderer, texture);
-
-            SDL_Delay(1);
-        }
-
-        GGTextureManager::Destroy(textureManager);
-        GGEventEngine::Destroy(event);
-
-        GGRendererEngine::Destroy(renderer);
-
+    if (renderer == nullptr)
+    {
+        std::cerr << "Failed to create renderer" << std::endl;
         SDL_DestroyWindow(window);
         window = nullptr;
-
-        SDL_Quit();
-
-        return 0;
+        return -1;
     }
+
+    auto textureManager = GGTextureManager::Create();
+    auto handle = GGTextureManager::AddTexture(textureManager,
+    GGLoader::LoadTexture(GGRendererEngine::GetRenderer(renderer), "test.png"));
+
+    auto event = GG_CreateEvent();
+
+    auto running = true;
+
+    GG_RegisterKeyboardEvent(event, SDLK_ESCAPE, [&]() { running = false; });
+
+    while (running)
+    {
+        GGEventEngine::HandleEvents(event);
+
+        auto texture = GGTextureManager::GetTexture(textureManager, handle);
+        GGRendererEngine::Render(renderer, texture);
+
+        SDL_Delay(1);
+    }
+
+    GGTextureManager::Destroy(textureManager);
+    GG_DestroyEvent(event);
+
+    GGRendererEngine::Destroy(renderer);
+
+    SDL_DestroyWindow(window);
+    window = nullptr;
+
+    SDL_Quit();
+
+    return 0;
 }
