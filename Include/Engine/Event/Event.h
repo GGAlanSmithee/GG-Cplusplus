4dc0185d489f6a9e2bcbaad242f4dcb353d47533
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-02-04 10:33:50 +0000

    Updates Event.h to use new naming conventions

--- Include/Engine/Event/Event.h
+++ Include/Engine/Event/Event.h
@@ -5,46 +5,50 @@
 #include <unordered_map>
 #include <functional>
 
-namespace GGEventEngine
+
+class GG_Event
 {
-    class Instance
-    {
-        friend class InstanceAccessor;
+    public:
+        /// Creates an instance of the event engine
+        /// @author Alan Smithee
+        /// @date created 2015-02-01
+        /// @return a pointer to an event
+        friend GG_Event* Create();
 
-        Instance() { };
+        /// Destroys an instance by freeing the memory used
+        /// @author Alan Smithee
+        /// @date created 2015-02-01
+        /// @param event a pointer to the event to destroy
+        friend void Destroy(GG_Event*);
 
+        /// Polls for events and dispatches them to registered callbacks (if any)
+        /// @author Alan Smithee
+        /// @date created 2014-12-08
+        /// @date changed 2015-02-01
+        /// @param event the event used to handle events
+        friend void HandleEvents(GG_Event*);
+
+        /// Registers a callback to a keyboard event
+        /// @author Alan Smithee
+        /// @date created 2015-02-01
+        /// @param event the event used to handle events
+        /// @param keycode the keycode to bind the callback to
+        /// @param callback the callback to invoke when this event occours
+        friend void RegisterKeyboardEvent(GG_Event*, const SDL_Keycode, std::function<void()>);
+            
+        friend const SDL_Event GetEvent(GG_Event*);
+            
+    private:
+        GG_Event() { };
+        ~GG_Event() { };
+            
+        void HandleKeyDown(GG_Event*, const SDL_Keycode);
+        void HandleKeyUp(GG_Event*, const SDL_Keycode);
+            
         SDL_Event _event;
 
         std::unordered_map<SDL_Keycode, std::function<void()>> _keyDownCallbacks;
         std::unordered_map<SDL_Keycode, std::function<void()>> _keyUpCallbacks;
-    };
-
-    /// Creates an instance of the event engine
-    /// @author Alan Smithee
-    /// @date created 2015-02-01
-    /// @return a pointer to a instance object
-    Instance* Create();
-
-    /// Destroys an instance by freeing the memory used
-    /// @author Alan Smithee
-    /// @date created 2015-02-01
-    /// @param a pointer to the instance to destroy
-    void Destroy(Instance*);
-
-	/// Polls for events and dispatches them to registered callbacks (if any)
-	/// @author Alan Smithee
-	/// @date created 2014-12-08
-	/// @date changed 2015-02-01
-	/// @param instance the event engine instance used to handle events
-	void HandleEvents(Instance* instance);
-
-	/// Registers a callback to a keyboard event
-	/// @author Alan Smithee
-	/// @date created 2015-02-01
-	/// @param instance the event engine instance used to handle events
-	/// @param keycode the keycode to bind the callback to
-	/// @param callback the callback to invoke when this event occours
-	void RegisterCallbackToKeyboardEvent(Instance*, const SDL_Keycode, std::function<void()>);
-}
+};
 
 #endif // ENGINE_EVENT_EVENT_H_INCLUDED
