4d3345f0b01cf8b299582d8f76584fe868a4c28a
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-02-04 11:38:30 +0000

    updates Core.h to use new naming conventions and updates its usage of the application class

--- Include/Engine/Core/Core.h
+++ Include/Engine/Core/Core.h
@@ -3,27 +3,24 @@
 
 #include "Application/Application.h"
 
-namespace GGCoreEngine
-{
-    /// Initialized SDL
-    /// @author Alan Smithee
-    /// @date created 2015-02-01
-    /// @param flags the flags used to initialize SDL
-    const bool InitializeSDL(const int);
+/// Initialized SDL
+/// @author Alan Smithee
+/// @date created 2015-02-01
+/// @param flags the flags used to initialize SDL
+const bool GG_InitializeSDL(const int);
 
-    /// Initialized SDL image
-    /// @author Alan Smithee
-    /// @date created 2015-02-01
-    /// @param flags the flags used to initialize SDL image
-    const bool InitializeSDLImage(const int);
+/// Initialized SDL image
+/// @author Alan Smithee
+/// @date created 2015-02-01
+/// @param flags the flags used to initialize SDL image
+const bool GG_InitializeSDLImage(const int);
 
-    /// Executes the core engine
-    /// @author Alan Smithee
-    /// @date created 2014-12-03
-    /// @date changed 2015-02-01
-    /// @param application the application that is using the engine
-    /// @remarks runs the main loop by using other engine's functionality
-    const int Execute(GGApplication::Application&);
-}
+/// Executes the core engine
+/// @author Alan Smithee
+/// @date created 2014-12-03
+/// @date changed 2015-02-01
+/// @param application the application that is using the engine
+/// @remarks runs the main loop by using other engine's functionality
+const int GG_Execute(GG_Application&);
 
 #endif // ENGINE_CORE_CORE_H_INCLUDED
