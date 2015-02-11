9b018ffcbae4a54632b06ed7aa834c122bb12872
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-02-11 20:04:56 +0100

    removes ECS in preparation for integrating an existing framework for handling entities

--- Include/Application/Application.cpp
+++ Include/Application/Application.cpp
@@ -1,93 +1,18 @@
 #include "Application.h"
 
-GG_Application::GG_Application(GG_World* world) :
-    _world(world)
+GG_Application::GG_Application()
 {
     // Empty
 }
 
 GG_Application::~GG_Application()
 {
-    if (_world == nullptr)
-    {
-        return;
-    }
-
-    delete _world;
-    _world = nullptr;
+    // Empty
 }
 
-void GG_Application::CameraSystem()
+GG_Application* const GG_CreateApplication()
 {
-    GGCoreEngine::Display*     d;
-    GGCoreEngine::Focus*       f;
-    GGCoreEngine::PointOfView* p;
-    GGCoreEngine::Transform*   t;
-
-    for (unsigned int entity = 0; entity < _world->Size; ++entity)
-    {
-        if ((_world->Enteties[entity] & CameraMask) != CameraMask)
-        {
-            continue;
-        }
-
-        d = &(_world->DisplayComponents[entity]);
-        f = &(_world->FocusComponents[entity]);
-        p = &(_world->PointOfViewComponents[entity]);
-        t = &(_world->TransformComponents[entity]);
-
-        /// @todo use camera here
-    }
-}
-
-void GG_Application::MovementSystem()
-{
-    GGCoreEngine::Transform* transform;
-    GGCoreEngine::Physics*     physics;
-
-    for (auto entity = 0; entity < _world->Size; ++entity)
-    {
-        if ((_world->Enteties[entity] & MovementMask) != MovementMask)
-        {
-            continue;
-        }
-
-        transform = &(_world->TransformComponents[entity]);
-        physics     = &(_world->PhysicsComponents[entity]);
-
-//        transform->Scale = glm::scale(glm::mat4(0.2f), glm::vec3(0.5f, 0.5f, 0.5f));
-//        transform->Rotation = glm::rotate(glm::mat4(1.0f), 76.0f, glm::vec3(0.75f, 1.0f, 0.302f));
-//        transform->Translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
-    }
-}
-
-void GG_Application::RenderingSystem()
-{
-    GGCoreEngine::Transform*  t;
-    GGCoreEngine::Appearance* a;
-
-    for (unsigned int entity = 0; entity < _world->Size; ++entity)
-    {
-        if ((_world->Enteties[entity] & RenderMask) != RenderMask)
-        {
-            continue;
-        }
-
-        t = &(_world->TransformComponents[entity]);
-        a = &(_world->AppearanceComponents[entity]);
-
-//        GGRendererEngine::DrawModel(t->Get(), a->Scene, a->Shader);
-    }
-}
-
-GG_Application* const GG_CreateApplication(GG_World* const world)
-{
-    if (world == nullptr)
-    {
-        return new GG_Application(new GG_World(1000));
-    }
-
-    return new GG_Application(world);
+    return new GG_Application();
 }
 
 void GG_DestroyApplication(GG_Application* application)
@@ -103,15 +28,15 @@ void GG_DestroyApplication(GG_Application* application)
 
 void GG_OnEvent(GG_Application* const application, GG_Engine* const engine)
 {
-
+    // Empty
 }
 
 void GG_OnLogic(GG_Application* const application, GG_Engine* const engine)
 {
-
+    // Empty
 }
 
 void GG_OnRender(GG_Application* const application, GG_Engine* const engine)
 {
-
+    // Empty
 }
--- Include/Application/Application.h
+++ Include/Application/Application.h
@@ -1,18 +1,12 @@
 #ifndef APPLICATION_APPLICATION_H_INCLUDED
 #define APPLICATION_APPLICATION_H_INCLUDED
 
-#include "Engine/Core/Component.h"
-#include "Engine/Core/World.h"
 #include "Engine/Core/Core.h"
-#include <bitset>
 
-using Mask = std::bitset<GGCoreEngine::NumbComponentTypes>;
-
-/// @todo change name of this to something more fitting
 class GG_Application
 {
     public:
-        friend GG_Application* const GG_CreateApplication(GG_World* const);
+        friend GG_Application* const GG_CreateApplication();
         friend void GG_DestroyApplication(GG_Application*);
 
         friend void GG_OnEvent(GG_Application* const, GG_Engine* const);
@@ -21,34 +15,17 @@ class GG_Application
 
     private:
 
-        GG_Application(GG_World*);
+        GG_Application();
         ~GG_Application();
-
-        void CameraSystem();
-        void MovementSystem();
-        void RenderingSystem();
-
-        const Mask CameraMask = GGUtility::ToIntegral(GGCoreEngine::ComponentType::Transform)   |
-                                GGUtility::ToIntegral(GGCoreEngine::ComponentType::Focus)       |
-                                GGUtility::ToIntegral(GGCoreEngine::ComponentType::PointOfView) |
-                                GGUtility::ToIntegral(GGCoreEngine::ComponentType::Display);
-
-        const Mask MovementMask = GGUtility::ToIntegral(GGCoreEngine::ComponentType::Transform) |
-                                  GGUtility::ToIntegral(GGCoreEngine::ComponentType::Physics);
-
-        const Mask RenderMask = GGUtility::ToIntegral(GGCoreEngine::ComponentType::Transform) |
-                                GGUtility::ToIntegral(GGCoreEngine::ComponentType::Appearance);
-
-        GG_World* _world = nullptr;
 };
 
 /// Creates a GG_Application* instance
 /// @author Alan Smithee
 /// @date created 2015-02-10
-/// @param world the GG_World* of the GG_Application
+/// @date changed 2015-02-11
 /// @return a GG_Application* instance
 /// @remarks friend of GG_Application
-GG_Application* const GG_CreateApplication(GG_World* const);
+GG_Application* const GG_CreateApplication();
 
 /// Destroys a GG_Application* instance
 /// @author Alan Smithee
