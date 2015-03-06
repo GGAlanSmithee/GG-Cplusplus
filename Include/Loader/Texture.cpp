89383e375726e902501f63a65fb1d697096c9585
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-03-06 12:44:59 +0000

    adds color keying when loading a texture

--- Include/Loader/Texture.cpp
+++ Include/Loader/Texture.cpp
@@ -25,6 +25,8 @@ SDL_Texture* const GG_LoadTexture(std::unique_ptr<GG_TextureLoader> const& textu
         throw file_not_found_error(IMG_GetError());
     }
 
+    SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 0xFF, 0, 0xFF));
+    
     auto texture = SDL_CreateTextureFromSurface(GG_GetSDLRenderer(renderer), surface);
 
     SDL_FreeSurface(surface);
