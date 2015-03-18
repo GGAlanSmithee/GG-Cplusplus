633a30a6c7163b6e8b56537f6b583e66623ecaac
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-03-18 07:51:50 +0000

    adds functionality to clean up children in gui element class

--- Include/Gui/Element.cpp
+++ Include/Gui/Element.cpp
@@ -3,18 +3,27 @@
 GG_GUI_Element::GG_GUI_Element() :
     GG_GUI_Element({ 0.0f, 0.0f, 0.0f, 0.0f })
 {
-
+    // Empty
 }
 
 GG_GUI_Element::GG_GUI_Element(GG_Rect const& rect) :
     _rect(rect)
 {
-
+    // Empty
 }
 
 GG_GUI_Element::~GG_GUI_Element()
 {
-    // Empty
+    for (auto child : _children)
+    {
+        if (child == nullptr)
+        {
+            continue;
+        }
+        
+        delete child;
+        child = nullptr;
+    }
 }
 
 void GG_Render(GG_GUI_Element const& element, std::unique_ptr<GG_Renderer> const& renderer)
