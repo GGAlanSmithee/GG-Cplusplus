c40298c67bd3bd6b1a2dde8be0e386f984ca97ec
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-03-20 09:42:09 +0000

    changes styles to a map of styles identified by a string and defines add / remove style functions

--- Include/Gui/Element.h
+++ Include/Gui/Element.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_map>
 #include "Math/Vec2.h"
 #include "Math/Rect.h"
 #include "Engine/Renderer.h"
@@ -18,20 +19,18 @@ enum class GG_GUI_Style
 class GG_GUI_Element
 {
     public:
-      GG_GUI_Element();
-      GG_GUI_Element(GG_Rect const&);
-      ~GG_GUI_Element();
+        GG_GUI_Element();
+        GG_GUI_Element(GG_Rect const&);
+        ~GG_GUI_Element();
 
-      friend void GG_Render(GG_GUI_Element const&, std::unique_ptr<GG_Renderer> const&);
-
+        friend void GG_Render(GG_GUI_Element const&, std::unique_ptr<GG_Renderer> const&);
+        friend void GG_AddStyle(GG_GUI_Element const&, std::string const&, GG_GUI_Style const&);
+        friend void GG_RemoveStyle(GG_GUI_Element const&, std::string const&, GG_GUI_Style const&);
+        
     private:
         GG_Rect _rect;
 
-        struct Style
-        {
-            GG_GUI_Style position;
-            GG_GUI_Style size;
-        };
+        std::unordered_map<std::string, GG_GUI_Style> _styles;
 
         GG_GUI_Element* _parent = nullptr;
         std::vector<GG_GUI_Element*> _children;
